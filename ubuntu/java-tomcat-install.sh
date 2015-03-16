6e092bc72dd7254657d91ccbdb48df6095edce4c
snallami <snallami@gmail.com>
2015-03-16 11:58:56 -0700

    ubuntu template

---
+++ ubuntu/java-tomcat-install.sh
@@ -0,0 +1,8 @@
+# Install Java
+sudo apt-get -y update
+sudo apt-get install -y openjdk-7-jdk
+sudo apt-get -y update --fix-missing
+sudo apt-get install -y openjdk-7-jdk
+
+# Install tomcat
+sudo apt-get install -y  tomcat7
