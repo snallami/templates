d8cb76bacdd715acd23fc3d0825dbbe66733ac29
snallami <snallami@gmail.com>
2015-03-18 18:06:58 -0700

    Taking package names as parameters

--- ubuntu/java-tomcat-install.sh
+++ ubuntu/java-tomcat-install.sh
@@ -1,8 +1,8 @@
 # Install Java
 sudo apt-get -y update
-sudo apt-get install -y openjdk-7-jdk
+sudo apt-get install -y $1
 sudo apt-get -y update --fix-missing
-sudo apt-get install -y openjdk-7-jdk
+sudo apt-get install -y $1
 
 # Install tomcat
-sudo apt-get install -y  tomcat7
+sudo apt-get install -y  $2
